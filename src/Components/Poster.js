bbc94b572a265a58a1d081e0f4cb20892efe41fc
Luka Ku <lukaszkuc93@gmail.com>
2022-01-12 11:10:37 +0100

    poster component

---
+++ src/Components/Poster.js
@@ -0,0 +1,9 @@
+const Poster = ({ posterSrc }) => {
+  return (
+    <div>
+      <img alt="poster" src={posterSrc}></img>
+    </div>
+  );
+};
+
+export default Poster;
--- src/moviesLibrary.js
+++ src/moviesLibrary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DescriptionFields from "./Components/DescriptionFields";
+import Poster from "./Components/Poster";
 import { MoviesList } from "./Movies-Data/moviesData";
 
 const MoviesLibrary = () => {
@@ -10,8 +11,14 @@ const MoviesLibrary = () => {
     <div>
       {filmsList.map((film) => (
         <div>
-          <h3>{film.Title}</h3>
+          <h3>
+            {film.Title} {film.Year}
+          </h3>
+          <Poster posterSrc={film.Poster} />
           <DescriptionFields label="Genre" value={film.Genre} />
+          <DescriptionFields label="Awards" value={film.Awards} />
+          <DescriptionFields label="Director" value={film.Director} />
+          <DescriptionFields label="Plot" value={film.Plot} />
         </div>
       ))}
     </div>
